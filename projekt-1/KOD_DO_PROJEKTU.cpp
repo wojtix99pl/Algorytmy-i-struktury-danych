4fc9aa57d7d00f9c6f54316f4dd9918bf7549d22
wojtix99pl <swiderwojciech93@gmail.com>
2024-11-03 14:49:16 +0100

    Added chart_1, moved algorithm into function algorytm

--- projekt-1/KOD_DO_PROJEKTU.cpp
+++ projekt-1/KOD_DO_PROJEKTU.cpp
@@ -5,17 +5,12 @@
 #include <chrono>
 
 using namespace std;
-int main()
-{
 
-    int suma = 1500;
-    int dlugosc_tablicy = 0;
+void algorytm(int dlugosc_tablicy, int suma) {
     int maxDlugosc = 0;
     int maxStart =0;
     int maxStop = 0;
     bool pierwszyZnaleziony = false;
-    cout<<"podaj dlugosc tablicy"<<endl;
-    cin>> dlugosc_tablicy;
     auto start = std::chrono::high_resolution_clock::now();
     int tablica[dlugosc_tablicy];
     srand(time(0));
@@ -27,37 +22,37 @@ int main()
     }
     for(int i =0; i  < dlugosc_tablicy; i++)
     {
-    if(i==0) cout<<"Podciagi o sumie "<< suma<<" to: ";
-    int sumaPomocnicza = 0;
-     for(int j = i; j < dlugosc_tablicy; j++ )   
-     {   sumaPomocnicza += tablica[j];
-        if(sumaPomocnicza == suma)
-        {
-            if(!pierwszyZnaleziony)
+        if(i==0) cout<<"Podciagi o sumie "<< suma<<" to: ";
+        int sumaPomocnicza = 0;
+        for(int j = i; j < dlugosc_tablicy; j++ )
+        {   sumaPomocnicza += tablica[j];
+            if(sumaPomocnicza == suma)
             {
-                pierwszyZnaleziony = true;
-                cout<<"[";
-            }
-            else
-            {
-                cout<<", [";
-            }
-            for(int x = i; x <= j; x++)
-            {
-                if(x==j)cout<<tablica[x];
-                else cout<<tablica[x]<<",";
-            }
-            
-            cout<<"]";
-            int dlugoscPodciagu = j + 1 - i;
-            if( dlugoscPodciagu > maxDlugosc )
-            {
-                maxStart = i;
-                maxStop = j;
-                maxDlugosc = dlugoscPodciagu;
+                if(!pierwszyZnaleziony)
+                {
+                    pierwszyZnaleziony = true;
+                    cout<<"[";
+                }
+                else
+                {
+                    cout<<", [";
+                }
+                for(int x = i; x <= j; x++)
+                {
+                    if(x==j)cout<<tablica[x];
+                    else cout<<tablica[x]<<",";
+                }
+
+                cout<<"]";
+                int dlugoscPodciagu = j + 1 - i;
+                if( dlugoscPodciagu > maxDlugosc )
+                {
+                    maxStart = i;
+                    maxStop = j;
+                    maxDlugosc = dlugoscPodciagu;
+                }
             }
         }
-     }
     }
     if(maxDlugosc!=0) {
         cout<<"."<<endl<<"Najdluzszy podciag to: "<<"[";
@@ -76,6 +71,18 @@ int main()
     auto end = std::chrono::high_resolution_clock::now();
     chrono::duration<double,milli> elapsed = end - start;
     cout<<elapsed.count();
+
+}
+int main()
+
+{
+
+    int suma = 1500;
+    int dlugosc_tablicy = 0;
+    cout<<"podaj dlugosc tablicy"<<endl;
+    cin>> dlugosc_tablicy;
+    algorytm(dlugosc_tablicy, suma);
+
 
     return 0;
 }
